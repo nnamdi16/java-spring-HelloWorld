9c4b45a0117d58f178da89bf4a3d49d2caae4ea5
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-09-04 23:06:06 +0100

    Injecting the English version by type, the french from the name, the german is derieved from the method name

--- src/main/java/com/example/demo/config/HelloConfig.java
+++ src/main/java/com/example/demo/config/HelloConfig.java
@@ -4,6 +4,7 @@ import com.example.demo.services.HelloWorldFactory;
 import com.example.demo.services.HelloWorldService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
@@ -17,19 +18,28 @@ public class HelloConfig {
 
     @Bean
     @Profile("default | english")
+    @Primary
     public HelloWorldService helloWorldServiceEnglish(HelloWorldFactory factory) {
         return factory.createHelloWorldService("en");
     }
 
     @Bean
     @Profile("spanish")
+    @Primary
     public HelloWorldService helloWorldServiceSpanish(HelloWorldFactory factory) {
         return factory.createHelloWorldService("es");
     }
 
-    @Bean
-    @Profile("french")
+    @Bean(name = "french")
+//    @Profile("french")
     public HelloWorldService helloWorldServiceFrench(HelloWorldFactory factory) {
         return factory.createHelloWorldService("fr");
     }
+
+    @Bean
+//    @Profile("french")
+    public HelloWorldService helloWorldServiceGerman(HelloWorldFactory factory) {
+        return factory.createHelloWorldService("ge");
+    }
+
 }
